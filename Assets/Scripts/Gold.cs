e40d05d8f9506d5af4d03f83669ece86fb571b7c
Tobias <tobiasbjerregaard@live.dk>
2021-05-01 22:25:10 +0200

    Tilføjet gold pickup + scripts

---
+++ Assets/Scripts/Gold.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gold : MonoBehaviour
+{
+
+    private GoldManager pm;
+    // Her laver jeg en public hvor jeg kan vælge hvilken lydfil der skal spilles ved opsamling af mønt
+    public AudioClip PickUpAudioClip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    pm = GameObject.Find("GoldManager").GetComponent<GoldManager>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            // Her bestemmer man værdien af points
+           pm.AddPoint(1);
+            // Her sørger vi for at clippet som vi refferede til i public bliver spillet. Når mønten bliver taget op. Transform position betyder bare clippet bliver spillet hvor vi er.
+          AudioSource.PlayClipAtPoint(PickUpAudioClip, transform.position);
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/GoldManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+    // Vi bruger TMPro for at lave en public TextMesh
+using TMPro;
+
+public class GoldManager : MonoBehaviour
+{
+    public int Point = 0;
+    // Laver en refference til manager text
+    public TextMeshProUGUI PointText;
+
+    public int PointToEnableTarget;
+
+    public GameObject TargetToEnable;
+
+    public void AddPoint(int amount)
+    {
+        // Tilføjer 1 point til det nuværende amount
+        Point += amount;
+        // Her sørger vi for at Points: ændre sig. Ved at lave en string beholder vi selve ordets points ellers ville det bare være nummeret.
+        PointText.text = "" + Point;
+        // Her gør vi så hvis point eller flere som man har brug for TargetToEnable så bliver vores goal activ
+        if(Point >= PointToEnableTarget)
+        {
+            TargetToEnable.SetActive(true);
+        }
+    }
+
+}
