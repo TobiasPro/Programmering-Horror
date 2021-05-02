18683867e94b403fd9095fa9336af1cd182765b9
Tobias <tobiasbjerregaard@live.dk>
2021-05-02 19:36:51 +0200

    Tilføjet EndGame Scene + Music

---
+++ Assets/Scripts/GameComplete.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameComplete : MonoBehaviour
+{
+    // Refference til TimeManager
+    public GameComplete Goal;
+
+    public int NextLevel;
+    public float TimeUntilNextLevel;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            StartCoroutine(WaitAndChangeLevel());
+        }
+    }
+
+
+    IEnumerator WaitAndChangeLevel()
+    {
+        yield return new WaitForSeconds(TimeUntilNextLevel);
+        SceneManager.LoadScene(NextLevel);
+    }
+
+}
---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+   public void Quit()
+   {
+       Application.Quit();
+   }
+
+    public void LoadLevel(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+}
